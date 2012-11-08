f78fd8b1ad581dd6f5ccbd0d7c4abf850af98898
misterrr <misterrr@GX280>
2012-11-08 00:31:22 +0000

    Initial commit

---
+++ config/config.php.sample.php
@@ -0,0 +1,16 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+
+define('DB_NAME', 'database');
+define('DB_USER', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', 'localhost');
+
+define('BASE_PATH','http://site_root');
+
+
+define('PAGINATE_LIMIT', '5');

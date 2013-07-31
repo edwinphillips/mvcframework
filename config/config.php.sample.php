dc6d6ec0fe9d9135da0056ac8519199a310cb274
Edwin Phillips <edwin.phillips@catalyst-eu.net>
2013-07-31 15:30:30 +0100

    Update config.php.sample.php

--- config/config.php.sample.php
+++ config/config.php.sample.php
@@ -1,16 +1,10 @@
 <?php
 
 /** Configuration Variables **/
-
-define ('DEVELOPMENT_ENVIRONMENT',true);
-
-
-define('DB_NAME', 'database');
-define('DB_USER', 'username');
-define('DB_PASSWORD', 'password');
-define('DB_HOST', 'localhost');
-
-define('BASE_PATH','http://site_root');
-
-
-define('PAGINATE_LIMIT', '5');
+define('DEVELOPMENT_ENVIRONMENT', true);
+define('DB_NAME',                 'database');
+define('DB_USER',                 'username');
+define('DB_PASSWORD',             'password');
+define('DB_HOST',                 'localhost');
+define('BASE_PATH',               'http://site_root');
+define('PAGINATE_LIMIT',          '5');
